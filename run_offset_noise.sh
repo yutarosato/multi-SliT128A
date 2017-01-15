796c7b3a3b095fcbe6af063eb211ae7f85b6685c
SliT <SliT@mar89-162.kek.jp>
2017-01-15 18:41:37 +0900

    suppression for other chip

--- run_offset_noise.sh
+++ run_offset_noise.sh
@@ -1,13 +1,14 @@
 #! /bin/tcsh -f
 
-if( $#argv < 2 )then
-    echo " Usage : $0 [ch] [DAC]"
-    echo "Example: $0  60    31"
+if( $#argv < 3 )then
+    echo " Usage : $0 [chip] [ch] [DAC]"
+    echo "Example: $0   0     60   31"
   exit 1
 endif
 
-set CH       = $1
-set CTRL_DAC = $2
+set CHIP     = $1
+set CH       = $2
+set CTRL_DAC = $3
 
 (cd decoder; make;)
 
@@ -19,14 +20,36 @@ set TMP = `echo "obase=2; ibase=10; ${CTRL_DAC}" | bc | sed 's|-||'`
       endif
 echo "   DAC : ${CTRL_DAC} => ${CTRL_DAC_BIT}"
 
-# <Slow Control>
-cd slow_control;
-./make_control.sh 0000000 $CH LLLLL${CTRL_DAC_BIT}LLHHH LLLLL${CTRL_DAC_BIT}LLLLL
 
-while (1)
-   ./slit128sc control.dat 192.168.10.16;
-   if( $? == 0 ) then
-      break
-   endif
+foreach ICHIP( 0 1 2 3 )
+    if( ${ICHIP} == 0 ) then
+	set CTRL_CHIP = "0000000"
+    else if( ${ICHIP} == 1 ) then
+	set CTRL_CHIP = "0000001"
+    else if( ${ICHIP} == 2 ) then
+	set CTRL_CHIP = "0000010"
+    else if( ${ICHIP} == 3 ) then
+	set CTRL_CHIP = "0000011"
+    else
+	echo "Wrong CHIP-ID : ${CHIP}"
+	exit
+    endif
+
+    # <Slow Control>
+    cd slow_control;
+    if( ${ICHIP} == ${CHIP} ) then
+       ./make_control.sh ${CTRL_CHIP} ${CH} LLLLL${CTRL_DAC_BIT}LLHHH LLLLL${CTRL_DAC_BIT}LLLLL
+    else
+       ./make_control.sh ${CTRL_CHIP} ${CH} LLLLL${CTRL_DAC_BIT}LLLLL LLLLL${CTRL_DAC_BIT}LLLLL
+    endif
+
+    while (1)
+     ./slit128sc control.dat 192.168.10.16;
+     if( $? == 0 ) then
+        break
+     endif
+    end
+    #./slit128sc -d control.dat 192.168.10.16;
+    cd ../
+    echo $ICHIP
 end
-#./slit128sc -d control.dat 192.168.10.16;
